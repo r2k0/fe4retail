095754d4203ee20203000422d2bc87742f92be43
r2k0 <iheartcpp@gmail.com>
2014-09-17 09:01:45 -0700

    removing redundant lic statements

--- lechef/lechef/qstk_tester.py
+++ lechef/lechef/qstk_tester.py
@@ -1,24 +1,5 @@
 """
 To test QSTK is working properly
-
-Distributed under MIT License. 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
 """
 
 import QSTK.qstkutil.qsdateutil as du
@@ -34,3 +15,8 @@ dt_start = dt.datetime(2010,1,1)
 dt_end = dt.datetime(2014,1,1)
 dt_timeofday = dt.timedelta(hours=16)
 ltd_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+c_dataobj = da.DataAccess('Yahoo')
+ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
+ldf_data = c_dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys)
+d_data = dict(zip(ls_keys, ldf_data))
