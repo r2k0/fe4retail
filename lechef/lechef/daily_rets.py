25963b2896330a856bbab9799fddd6f4dc51699a
r2k0 <iheartcpp@gmail.com>
2014-09-19 21:07:08 -0700

    adding plot_test and updates

--- lechef/lechef/daily_rets.py
+++ lechef/lechef/daily_rets.py
@@ -7,23 +7,31 @@ This is just for Python tutorial and demonstration.
 import numpy as np
 import pandas as pd
 from parse import parse
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
-
-
-def calculate_daily(data):
+def chart_daily(data):
 	"""	calculate daily returns """
 	# r(t) = ( price(t)/price(t-1) ) - 1
 
 	daily_ret = 0
 	prev_close = 0
+	dates = []
+	drl = []
 	for kv in data:
+		d = kv["Date"]
 		if prev_close > 0: 
 			daily_ret = (float(kv["Close"]) / prev_close) - 1
 			daily_ret = round(daily_ret,5) * 100
-		print kv["Date"], kv["Close"], daily_ret
+		print d, kv["Close"], daily_ret
+		drl.append(daily_ret)
+		dates.append(d)
 		prev_close = float(kv["Close"])
-	
+	#for i in drl: print i
+	x = tuple(dates)
+	plt.plot(drl)
+	plt.savefig("test.png")	
 
 
 def daily_returns():
@@ -34,7 +42,7 @@ def daily_returns():
 	spy_data.reverse()	
 	#qqq_data.reverse()
 	#dia_data.reverse()
-	calculate_daily(spy_data)
+	chart_daily(spy_data)
 
 def cumm_daily_returns():
 	""" cummulative daily returns """
