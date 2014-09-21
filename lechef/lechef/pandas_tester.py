b29fd93bf08c70102c7522fc04395d8bfa56fd1e
r2k0 <iheartcpp@gmail.com>
2014-09-21 14:24:43 -0700

    pandas update

--- lechef/lechef/pandas_tester.py
+++ lechef/lechef/pandas_tester.py
@@ -5,36 +5,38 @@ Copyright(c) 2014 Okkar Than
 This is just for Python tutorial and demonstration.
 To test if data sources are working properly using pandas
 """
+
 from pandas.io.data import DataReader
 import numpy as np
 import pandas as pd
 
 from datetime import datetime
-starting_date = datetime(2014,1,1)
-ending_date = datetime(2014,8,7)
+starting_date = datetime(2013,1,1)
+ending_date = datetime(2013,12,1)
 
 
-def readIndexData(ticker):
-	ticker_df = DataReader(ticker, "yahoo", starting_date, ending_date)
-	#del ticker_df['Volume']
-	return ticker_df
+def readTickerData(ticker):
+    ticker_df = DataReader(ticker, "yahoo", starting_date, ending_date)
+    del ticker_df['Volume']
+    del ticker_df['Open']
+    del ticker_df['High']
+    del ticker_df['Low']
+    del ticker_df['Adj Close']
+    return ticker_df
 
-"""
+def daily_return(t_df):
+    dlyr = t_df['Close'].shift(1) / t_df['Close'] - 1
+    t_df['Daily Return'] = dlyr * 100
+    return t_df
+    
+spy = readTickerData("SPY")
+
 print "SPY historical data"
-print readIndexData("SPY")[:5] 
+print spy.tail()
 
+spy_d = daily_return(spy)
+print "SPY daily retrun %"
+print spy_d.tail()
 
-print "QQQ historical data"
-print readIndexData("QQQ")[:5]
-
-print "DIA historical data"
-print readIndexData("DIA")[:5]
-"""
-"""
-s = pd.Series(np.random.rand(4), name='dreturns sample')
-print s
-print s.describe()
-"""
-
-spy = readIndexData("SPY")
-print spy.describe()
+print "SPY standard deviations of close prices and daily returns"
+print spy_d.std()
