b9c5870a796eb6d7ccc313269798911d45a76a6e
r2k0 <iheartcpp@gmail.com>
2014-09-22 17:15:17 -0700

    updates

--- lechef/lechef/chart_daily_rets.py
+++ lechef/lechef/chart_daily_rets.py
@@ -2,44 +2,48 @@
 The MIT License (MIT)
 Copyright(c) 2014 Okkar Than
 
-This is just for Python tutorial and demonstration.
-To test if data sources are working properly using pandas
 """
 
 from pandas.io.data import DataReader
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 from datetime import datetime
 starting_date = datetime(2013,1,1)
 ending_date = datetime(2013,12,1)
 
 
-def readTickerData(ticker):
-    """ read equity data and return as dataframe """
-    ticker_df = DataReader(ticker, "yahoo", starting_date, ending_date)
-    del ticker_df['Volume']
-    del ticker_df['Open']
-    del ticker_df['High']
-    del ticker_df['Low']
-    del ticker_df['Adj Close']
-    return ticker_df
-
-def daily_return(t_df):
-    """ add daily return column to the data frame """
-    dlyr = t_df['Close'].shift(1) / t_df['Close'] - 1
-    t_df['Daily Return'] = dlyr * 100
-    return t_df
-    
+   
 def chart_data(ticker):
+    
+    def readTickerData(ticker):
+        """ read equity data and return as dataframe """
+        ticker_df = DataReader(ticker, "yahoo", starting_date, ending_date)
+        del ticker_df['Volume']
+        del ticker_df['Open']
+        del ticker_df['High']
+        del ticker_df['Low']
+        del ticker_df['Adj Close']
+        return ticker_df
+    
+    def daily_return(t_df):
+        """ add daily return column to the data frame """
+        dlyr = t_df['Close'].shift(1) / t_df['Close'] - 1
+        t_df['Daily Return'] = dlyr * 100
+        return t_df
+     
+
     d = readTickerData(ticker)
     df = daily_return(d)
 
     # Standard Deviation of Close and Daily Return
     print ticker,"standard deviations of close prices and daily returns"
     print df.std()
+    del df['Close']
     return df
+
 
 
 def main():
@@ -47,36 +51,23 @@ def main():
     qqq_d = chart_data("QQQ")
     dia_d = chart_data("DIA")
     tlt_d = chart_data("TLT")
-    # plot daily return percent
-    #spy_d.plot(y='Daily Return')
-    #plt.axhline(0,color='g')
-    #plt.savefig('spy.png')
+    
     fig = plt.figure()
-    ax1 = fig.add_subplot(2,2,1)
-    ax2 = fig.add_subplot(2,2,2)
-    ax3 = fig.add_subplot(2,2,3)
-    ax4 = fig.add_subplot(2,2,4)
+    gs = gridspec.GridSpec(2,2)
+    ax_list=[fig.add_subplot(ss) for ss in gs]
 
+    ax_list[0].plot(spy_d)
+    ax_list[1].plot(qqq_d)
+    ax_list[2].plot(dia_d)
+    ax_list[3].plot(tlt_d)
+
+    ax_list[0].set_title('SPY')
+    ax_list[1].set_title('QQQ')
+    ax_list[2].set_title('DIA')
+    ax_list[3].set_title('TLT')
+
+    plt.savefig('daily_return.png')
     
-    ax1.set_title('SPY Daily Returns')
-    spy_d.plot(ax=ax1,y='Daily Return')
-    plt.savefig('charts.png')
-
-    ax2.set_title('QQQ Daily Returns')
-    qqq_d.plot(ax=ax2,y='Daily Return')
-    plt.savefig('charts.png')
-
-    ax3.set_title('DIA Daily Returns')
-    dia_d.plot(ax=ax3,y='Daily Return')
-    plt.savefig('charts.png')
-
-    ax4.set_title('TLT Daily Returns')
-    tlt_d.plot(ax=ax4,y='Daily Return')
-  
-    # tight_layout automatically adjuts subplot params
-    plt.tight_layout()
-    
-    plt.savefig('charts.png')
 
 
 if __name__ == "__main__":
